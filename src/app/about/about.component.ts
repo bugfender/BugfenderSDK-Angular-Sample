106dc4edd65be7ff98a3663c700ce22d6239ff98
Aleix Ventayol <aleix@ventayol.cat>
2025-11-07 09:03:05 +0100

    Upgrade Angular and related dependencies to version 19

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,6 +7,7 @@ import { Bugfender } from '@bugfender/sdk';
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
+  standalone: false,
 })
 export class AboutComponent implements OnInit {
   version: string | null = environment.version;
